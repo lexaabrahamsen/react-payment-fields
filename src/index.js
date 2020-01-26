ce332ce885d0f6d8890bdc1147add2a9ce460ec1
Lexa <lexaraea@gmail.com>
2020-01-26 16:08:35 -0500

    Update designs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,99 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Styles from "./Styles";
+import { Form, Field } from "react-final-form";
+import Card from "./Card";
+import {
+  formatCreditCardNumber,
+  formatCVC,
+  formatExpirationDate
+} from "./cardUtils";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const onSubmit = async values => {
+  await sleep(300);
+  window.alert(JSON.stringify(values, 0, 2));
+};
+
+const App = () => (
+  <Styles>
+    <h1>React Credit Card Payment Form</h1>
+    <a href="https://google.com">GitHub</a>
+    <Form
+      onSubmit={onSubmit}
+      render={({
+        handleSubmit,
+        form,
+        submitting,
+        pristine,
+        values,
+        active
+      }) => {
+        return (
+          <form onSubmit={handleSubmit}>
+            <Card
+              number={values.number || ""}
+              name={values.name || ""}
+              expiry={values.expiry || ""}
+              cvc={values.cvc || ""}
+              focused={active}
+            />
+            <div>
+              <Field
+                name="number"
+                component="input"
+                type="text"
+                pattern="[\d| ]{16,22}"
+                placeholder="Card Number"
+                format={formatCreditCardNumber}
+              />
+            </div>
+            <div>
+              <Field
+                name="name"
+                component="input"
+                type="text"
+                placeholder="Name"
+              />
+            </div>
+            <div>
+              <Field
+                name="expiry"
+                component="input"
+                type="text"
+                pattern="\d\d/\d\d"
+                placeholder="Valid Thru"
+                format={formatExpirationDate}
+              />
+              <Field
+                name="cvc"
+                component="input"
+                type="text"
+                pattern="\d{3,4}"
+                placeholder="CVC"
+                format={formatCVC}
+              />
+            </div>
+            <div className="buttons">
+              <button type="submit" disabled={submitting}>
+                Submit
+              </button>
+              <button
+                type="button"
+                onClick={form.reset}
+                disabled={submitting || pristine}
+              >
+                Reset
+              </button>
+            </div>
+            <h2>Values</h2>
+            <pre>{JSON.stringify(values, 0, 2)}</pre>
+          </form>
+        );
+      }}
+    />
+  </Styles>
+);
+
+render(<App />, document.getElementById("root"));
